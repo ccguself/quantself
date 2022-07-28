540dc2e3dd865feb077b27d60889503d7499b617
ccguself <ccguself@163.com>
2022-07-28 08:48:24 +0800

    基础框架

---
+++ src/quantself/data/input.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+class Input:
+    def __init__(self):
+        return 
---
+++ src/quantself/data/raw_data.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import pandas as pd
+
+class RawData:
+    def __init__(self):
+        return
+
+
+
